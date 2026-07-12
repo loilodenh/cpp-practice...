b9089ad59a79353ad7c9cdf67fd72e58a1cc7b32
Nguyễn Doãn Trung <nguyendoantrung2012@gmail.com>
2026-07-12 21:39:43 +0700

    Create hello_world.cpp

---
+++ 01_Hello_World/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, UET!" << endl;
+    cout << "My name is loilodenh." << endl;
+    cout << "I will become a Mechatronics Engineer!" << endl;
+
+    return 0;
+}
